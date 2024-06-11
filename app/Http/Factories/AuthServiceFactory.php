daa8230e7f21d309c9aaa10af56c8cb9137ee8a2
Dawid Miklas <miklasdawid@gmail.com>
2024-06-11 15:55:06 +0200

    auth service security fix, removed unused class

--- app/Http/Factories/AuthServiceFactory.php
+++ app/Http/Factories/AuthServiceFactory.php
@@ -22,7 +22,13 @@ class AuthServiceFactory
      */
     public static function make(?string $system): AuthServiceContract
     {
-        if (!array_key_exists($system, self::MAP)) {
+        if (!array_key_exists($system, self::MAP) || !interface_exists(self::MAP[$system])) {
+            throw new InvalidSystemException();
+        }
+
+        $service = self::MAP[$system];
+
+        if (!is_subclass_of($service, AuthServiceContract::class)) {
             throw new InvalidSystemException();
         }
 
